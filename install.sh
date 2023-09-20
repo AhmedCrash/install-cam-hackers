5809fc040b25532d4a198da3d864735d27813b8b
Ahmed crash <82153523+AhmedCrash@users.noreply.github.com>
2023-09-21 01:44:56 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt-get install python3
+
+apt-get install git
+
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+
+cd Cam-Hackers
+
+pip install -r requirements.txt
+
+python3 cam-hackers.py
